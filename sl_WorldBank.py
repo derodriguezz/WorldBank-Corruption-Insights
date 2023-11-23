40e3aca2b12508cf45d93de5a12df0f99d696c85
JHAA56 <145810501+JHAA56@users.noreply.github.com>
2023-11-23 15:37:24 -0500

    complemento tabs s4

--- sl_WorldBank.py
+++ sl_WorldBank.py
@@ -17,36 +17,26 @@ st.image('https://thelogisticsworld.com/wp-content/uploads/2023/09/Cepal.jpg')
 
 # Configurar la barra lateral con las pestañas
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 # Configurar la barra lateral con las pestañas
 st.sidebar.title('Pestañas')
 opciones = ['Pestaña 1', 'Pestaña 2', 'Pestaña 3']
 seleccion = st.sidebar.radio('Ir a:', opciones)
 
-# Cargar datos desde el archivo
-ruta_archivo = r'C:\JHAA\CEPAL_3\WorldBank-Corruption-Insights\Extraccion\structured_data\reduced_df_normalized.xlsx'
-df = pd.read_excel(ruta_archivo)
-
 # Contenido de las pestañas
 if seleccion == 'Pestaña 1':
     st.subheader('Contenido de la Pestaña 1')
 
-    # Agregar aquí el contenido específico de la pestaña 1
-    st.write("Gráfico de barras:")
-    columna_seleccionada = st.selectbox("Seleccione una columna:", df.columns)
-    
-    # Crear un gráfico de barras
-    fig, ax = plt.subplots()
-    sns.countplot(x=columna_seleccionada, data=df, ax=ax)
-    st.pyplot(fig)
+    # Agrega aquí el contenido específico de la pestaña 1
+
+    # Agregar la imagen desde la ruta especificada
+    ruta_imagen = r'C:/JHAA/CEPAL_3/WorldBank-Corruption-Insights/Procesamiento/graficas_sl/correlacion.png'
+    st.image(ruta_imagen, caption='Matriz de Correlación', use_column_width=True)
 
 elif seleccion == 'Pestaña 2':
     st.subheader('Contenido de la Pestaña 2')
-    # Agregar aquí el contenido específico de la pestaña 2
+    # Agrega aquí el contenido específico de la pestaña 2
 
 elif seleccion == 'Pestaña 3':
     st.subheader('Contenido de la Pestaña 3')
-    # Agregar aquí el contenido específico de la pestaña 3
+    # Agrega aquí el contenido específico de la pestaña 3
