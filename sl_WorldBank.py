ab237859c0a334ee9524ce041c8bc6f6a5f106ba
JHAA56 <145810501+JHAA56@users.noreply.github.com>
2023-11-23 18:37:14 -0500

    actualizacion1

--- sl_WorldBank.py
+++ sl_WorldBank.py
@@ -21,15 +21,15 @@ tab1, tab2, tab3 = st.tabs(["Correlacion", "Dispersion", "Matriz"])
 
 with tab1:
    st.header("Grafica de Correlación entre variables")
-   st.image("Procesamiento\graficas_sl\correlacion.png", width=900)
+   st.image("Procesamiento/graficas_sl/correlacion.png", width=900)
 
 with tab2:
    st.header("Grafica de Dispersion entre variables")
-   st.image("Procesamiento\graficas_sl\diagrama_dispersión.png", width=900)
+   st.image("Procesamiento/graficas_sl/diagrama_dispersión.png", width=900)
 
 with tab3:
    st.header("Tabla Matriz de Correlación")
-   ruta_matriz_correlacion = r'C:\JHAA\CEPAL_3\WorldBank-Corruption-Insights\Procesamiento\graficas_sl\matriz_correlacion.csv'
+   ruta_matriz_correlacion = r'C:/JHAA/CEPAL_3/WorldBank-Corruption-Insights/Procesamiento/graficas_sl/matriz_correlacion.csv'
    matriz_correlacion = pd.read_csv(ruta_matriz_correlacion, index_col=0)
    # Aplicar estilos para resaltar valores
    estilos = matriz_correlacion.style.background_gradient(cmap='coolwarm').highlight_null('red')
