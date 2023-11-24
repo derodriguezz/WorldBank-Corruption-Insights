2311ce12a8abfacecd3d82cd5c10ab8d6192f5f4
JHAA56 <145810501+JHAA56@users.noreply.github.com>
2023-11-24 12:28:25 -0500

    actualizacion5

--- sl_WorldBank.py
+++ sl_WorldBank.py
@@ -6,7 +6,7 @@ import os
 
 # Cargar y mostrar la imagen
 #imagen = cargar_imagen(ruta_imagen)
-st.image('https://thelogisticsworld.com/wp-content/uploads/2023/09/Cepal.jpg', width=300)
+st.image('https://thelogisticsworld.com/wp-content/uploads/2023/09/Cepal.jpg', width=500)
 
 # Título de la aplicación
 st.title('CEPAL - INDICADORES ODS-CORR')
@@ -14,26 +14,14 @@ st.title('CEPAL - INDICADORES ODS-CORR')
 # Configurar la barra lateral con las pestañas
 import streamlit as st
 
-tab1, tab2, tab3, tab4, tab5 = st.tabs(["Correlacion", "Dispersion1" , "Dispersion2" , "Dispersion3", "Matriz"])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["Correlacion", "Matriz", "Dispersion" , "Dispersion Unificada" , "Modelo de Regresion", ])
 
 with tab1:
    st.header("Grafica de Correlación entre variables")
+   tab1.subheader("De acuerdo a la condición de normalidad de las variables analizadas, se realizan los análisis de correlación Spearman o Pearson según el resultado obtenido en cada una. En todos los análisis se determina la correlación entre la variable objetivo identificada y las variables de corrupción más relevantes identificadas a partir de la matriz de correlación.")
    st.image("Procesamiento/graficas_sl/correlacion.png", width=500)
 
 with tab2:
-   st.header("Grafica de Dispersion entre variables")
-   st.image("Procesamiento/graficas_sl/diagrama_dispersión.png", width=500)
-
-with tab3:
-   st.header("Grafica de Dispersion entre variables")
-   st.image("Procesamiento/graficas_sl/diagrama_dispersión1.png", width=500)
-
-with tab4:
-   st.header("Grafica de Dispersion entre variables")
-   st.image("Procesamiento/graficas_sl/diagrama_dispersión.png", width=500)
-
-
-with tab5:
    st.header("Tabla Matriz de Correlación")
    ruta_matriz_correlacion = r'Procesamiento/graficas_sl/matriz_correlacion.csv'
    matriz_correlacion = pd.read_csv(ruta_matriz_correlacion, index_col=0)
@@ -42,6 +30,33 @@ with tab5:
    st.dataframe(estilos)
    # Mostrar la aplicación Streamlit
    st.write('Matriz de Correlación, entre variables de interes')
+
+
+with tab3:
+   st.header("Grafica de Dispersion entre variables")
+   tab3.subheader ("")
+   st.image("Procesamiento/graficas_sl/diagrama_dispersión.png", width=500)
+
+with tab4:
+   st.header("Grafica de Dispersion entre variables")
+   tab4.subheader ("")
+ 
+
+   
+ 
+with tab5:
+   st.header("Matriz Modelos de Regresion")
+   tab5.subheader("Se implementa el entrenamiento de modelos de regresión lineal para prever la variable objetivo 'GDE_SL.GDP.PCAP.EM.KD'. En el primer escenario, se realiza la división de muestras sin considerar la variable categórica 'Country', utilizando 'CRP_CC.EST', 'CRP_GE.EST', 'CRP_RL.EST', 'CRP_VA.EST' como variables independientes. En los otros dos escenarios, se incorpora 'Country' y se generan modelos adicionales, uno con el conjunto completo de datos globales y otro con un conjunto reducido, evaluando así la influencia de esta variable en las predicciones. Se utiliza la técnica de regresión lineal y se evalúa el rendimiento del modelo en conjuntos de entrenamiento y prueba.")
+   ruta_matriz_modelos = r'Procesamiento/graficas_sl/modelos_df.csv'
+   matriz_modelos = pd.read_csv(ruta_matriz_correlacion, index_col=0)
+   # Aplicar estilos para resaltar valores
+   estilos = matriz_modelos.style.background_gradient(cmap='coolwarm').highlight_null('red')
+   st.dataframe(estilos)
+   # Mostrar la aplicación Streamlit
+   st.write('Matriz modelos de regresion')
+
+
+
 
 # Ruta del archivo Excel
 file_path = "Extraccion/structured_data/reduced_df_normalized.xlsx"
