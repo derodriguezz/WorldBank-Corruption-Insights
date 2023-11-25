cff42e35035fef09c6c24be0a199ae746f2a4b7d
JHAA56 <145810501+JHAA56@users.noreply.github.com>
2023-11-25 09:52:54 -0500

    actualizacion9

--- Procesamiento/worldbank_pipeline.py
+++ Procesamiento/worldbank_pipeline.py
@@ -959,14 +959,14 @@ plt.show()
 fig, ax = plt.subplots(figsize=(10, 6))
 interaction_plot(x=reduced_df['CRP_RL.EST'], trace=reduced_df['Country'], response=reduced_df['GDE_SL.GDP.PCAP.EM.KD'], ax=ax)
 plt.show()
-### 6.1. Matriz de correlación
-corrm_concatenado
+#### 6.1. Matriz de correlación
+#corrm_concatenado
 ### 6.2. Comprobación de normalidad
-norm_concatenado
+#norm_concatenado
 ### 6.3. Análisis de correlación
-resc_concatenado
+#resc_concatenado
 ### 6.4. Modelos de regresión
-modelos_df
+#modelos_df
 ##visualizar modelo2 _sp
 # Crear un DataFrame con los valores originales y predichos
 comparison_df_sp = pd.DataFrame({'Valor Real': y_test_sp, 'Valor Predicho': y_pred_test_lr_sp})
@@ -1080,6 +1080,7 @@ plt.show()
 
 
 
+
 import streamlit as st
 import pandas as pd
 import plotly.express as px
@@ -1088,7 +1089,7 @@ import os
 
 # Cargar y mostrar la imagen
 #imagen = cargar_imagen(ruta_imagen)
-st.image('https://thelogisticsworld.com/wp-content/uploads/2023/09/Cepal.jpg', width=300)
+st.image('https://thelogisticsworld.com/wp-content/uploads/2023/09/Cepal.jpg', width=900)
 
 # Título de la aplicación
 st.title('CEPAL - INDICADORES ODS-CORR')
@@ -1096,17 +1097,14 @@ st.title('CEPAL - INDICADORES ODS-CORR')
 # Configurar la barra lateral con las pestañas
 import streamlit as st
 
-tab1, tab2, tab3 = st.tabs(["Correlacion", "Dispersion", "Matriz"])
+tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Correlacion", "Matriz de Correlación", "Dispersion" , "Resultados Modelos de Regresión" , "Modelo de Regresion", "Gráfica iteractiva"])
 
 with tab1:
    st.header("Grafica de Correlación entre variables")
-   st.image("Procesamiento/graficas_sl/correlacion.png", width=900)
+   tab1.subheader("De acuerdo a la condición de normalidad de las variables analizadas, se realizan los análisis de correlación Spearman o Pearson según el resultado obtenido en cada una. En todos los análisis se determina la correlación entre la variable objetivo identificada y las variables de corrupción más relevantes identificadas a partir de la matriz de correlación.")
+   st.image("Procesamiento/graficas_sl/correlacion.png", width=700)
 
 with tab2:
-   st.header("Grafica de Dispersion entre variables")
-   st.image("Procesamiento/graficas_sl/diagrama_dispersión.png", width=900)
-
-with tab3:
    st.header("Tabla Matriz de Correlación")
    ruta_matriz_correlacion = r'Procesamiento/graficas_sl/matriz_correlacion.csv'
    matriz_correlacion = pd.read_csv(ruta_matriz_correlacion, index_col=0)
@@ -1116,40 +1114,59 @@ with tab3:
    # Mostrar la aplicación Streamlit
    st.write('Matriz de Correlación, entre variables de interes')
 
-# Ruta del archivo Excel
-file_path = "Extraccion/structured_data/reduced_df_normalized.xlsx"
 
-# Cargar el archivo Excel en un DataFrame
-df2 = pd.read_excel(file_path)
+with tab3:
+   st.header("Grafica de Dispersion entre variables")
+   tab3.subheader ("")
+   st.image("Procesamiento/graficas_sl/diagrama_dispersión.png", width=900)
 
-# Mostrar la tabla en Streamlit
-#st.dataframe(df2)
+with tab4:
+   st.header("Resultados modelos de Regresión")
+   st.write ("En este análisis, hemos utilizado una serie de indicadores del Banco Mundial que se centran en la gobernanza y el desempeño de los gobiernos.")  
+   tab4.subheader ("Control de la Corrupción (CC.EST):")
+   st.write ("Control de la Corrupción evalúa la percepción de hasta qué punto el poder público se ejerce para beneficio privado, abarcando formas tanto menores como mayores de corrupción.")
+   tab4.subheader ("Efectividad del Gobierno (GE.EST):")
+   st.write  ("Efectividad del Gobierno mide la calidad de los servicios públicos, la independencia del servicio civil y la credibilidad del compromiso del gobierno con sus políticas.")
+   tab4.subheader ("Estado de Derecho - Cumplimiento de la ley (RL.EST):")
+   st.write  ("Estado de Derecho refleja la confianza y el cumplimiento de las reglas de la sociedad, incluyendo la aplicación de contratos, derechos de propiedad, la actuación policial y judicial, así como la probabilidad de crimen y violencia.")
+   tab4.subheader ("Voz y Rendición de Cuentas (VA.ESTb4):")
+   st.write ("Voz y Rendición de Cuentas evalúa la participación ciudadana en la selección del gobierno y la libertad de expresión, asociación y medios de comunicación.")
 
-#df2 = pd.read_excel(file_path_2)
+with tab5:
+   st.header("Matriz Modelos de Regresion")
+   tab5.subheader("Resultado - Tabla comparativa entre modelos empleados")
+   ruta_matriz_modelos = r'Procesamiento/graficas_sl/modelos_df.csv'
+   matriz_modelos = pd.read_csv(ruta_matriz_modelos, index_col=0)
+   # Aplicar estilos para resaltar valores
+   estilos = matriz_modelos.style.background_gradient(cmap='coolwarm').highlight_null('red')
+   st.dataframe(matriz_modelos)
+   # Mostrar la aplicación Streamlit
+   st.write('Matriz modelos de regresion')
 
-# Streamlit app
-st.title('ANALISIS DE DATOS - CEPAL')
-
-# Selector de variables
-selected_variables = st.multiselect('Seleccionar Variable(s):', df2.columns)
-
-# Filtro por país
-selected_countries = st.multiselect('Seleccionar País(es):', df2['Country'].unique())
-
-# Filtro por año
-selected_year = st.slider('Seleccionar Año:', min_value=df2['Year'].min(), max_value=df2['Year'].max(), value=(df2['Year'].min(), df2['Year'].max()))
-
-# Filtrar el DataFrame
-filtered_df = df2[(df2['Country'].isin(selected_countries)) & (df2['Year'] >= selected_year[0]) & (df2['Year'] <= selected_year[1])]
-
-# Aplicar filtro de variables seleccionadas
-if selected_variables:
-    filtered_df = filtered_df[selected_variables + ['Year', 'Country']]
-
-# Graficar con Plotly Express
-fig = px.line(filtered_df, x='Year', y=selected_variables, color='Country', title='Gráfica Interactiva')
-st.plotly_chart(fig)
-
-# Mostrar tabla de datos
-#st.dataframe(filtered_df)
+with tab6:
+   st.header("Grafica Iteractiva -Indicadores - Pais - Rango de tiempo")
+   tab6.subheader("Comportamiento de los indicadores por Pais - CELAP ")
+   # Ruta del archivo Excel
+   file_path = "Extraccion/structured_data/reduced_df_normalized.xlsx"
+   # Cargar el archivo Excel en un DataFrame
+   df2 = pd.read_excel(file_path)
+   # Mostrar la tabla en Streamlit
+   #st.dataframe(df2)
+   #df2 = pd.read_excel(file_path_2)
+   # Streamlit app
+   st.title('ANALISIS DE DATOS - CEPAL')
+   # Selector de variables
+   selected_variables = st.multiselect('Seleccionar Variable(s):', df2.columns)
+   # Filtro por país
+   selected_countries = st.multiselect('Seleccionar País(es):', df2['Country'].unique())
+   # Filtro por año
+   selected_year = st.slider('Seleccionar Año:', min_value=df2['Year'].min(), max_value=df2['Year'].max(), value=(df2['Year'].min(), df2['Year'].max()))
+   # Filtrar el DataFrame
+   filtered_df = df2[(df2['Country'].isin(selected_countries)) & (df2['Year'] >= selected_year[0]) & (df2['Year'] <= selected_year[1])]
+   # Aplicar filtro de variables seleccionadas
+   if selected_variables:
+      filtered_df = filtered_df[selected_variables + ['Year', 'Country']]
+   # Graficar con Plotly Express
+   fig = px.line(filtered_df, x='Year', y=selected_variables, color='Country', title='Gráfica Interactiva')
+   st.plotly_chart(fig)
 
