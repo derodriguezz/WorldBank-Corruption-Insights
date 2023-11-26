1e96c6a4cc8c80c2ac29ccc27ca867258a03917d
JHAA56 <145810501+JHAA56@users.noreply.github.com>
2023-11-25 21:23:39 -0500

    actualizacion14

--- sl_WorldBank.py
+++ sl_WorldBank.py
@@ -6,21 +6,32 @@ import os
 
 # Cargar y mostrar la imagen
 #imagen = cargar_imagen(ruta_imagen)
-st.image('https://thelogisticsworld.com/wp-content/uploads/2023/09/Cepal.jpg', width=900)
+st.image('https://thelogisticsworld.com/wp-content/uploads/2023/09/Cepal.jpg',  width=600 ) 
+
 
 # Título de la aplicación
-st.title('CEPAL - INDICADORES ODS-CORR')
+st.title('Corrupción y Desarrollo Económico en los Paises de la CEPAL')
 
 # Configurar la barra lateral con las pestañas
 import streamlit as st
 
-tab1, tab2, tab3 = st.tabs(["INTRODUCCION ", "RESULTADOS", "EXPLORA LOS DATOS"])
+tab1, tab2, tab3 = st.tabs(["INTRODUCCION ", "RESULTADOS RELACION ENTRE VARIABLES", "EXPLORA LOS DATOS"])
 with tab1:
    st.header("BIENVENIDO!!!!!")
-   tab1.subheader("!Saludos y bienvenido a nuestra aplicación especializada en el análisis de indicadores de la CELAP!")
-   tab1.subheader("Estamos emocionados de tenerte como parte de nuestra comunidad, donde la toma de decisiones informadas y estratégicas se convierte en una experiencia accesible y eficiente. Aquí, en nuestra aplicación, encontrarás un espacio diseñado para potenciar tu capacidad de comprender y utilizar los indicadores clave de la CELAP de manera efectiva.")
+   st.write("Esta aplicación web se presenta como una valiosa herramienta para comprender la relación entre la corrupción y el desarrollo económico. La información que se presenta parte de un proyecto de investigación respaldado por metodologías de Big Data, las cuales analizan los indicadores más significativos para modelar esta relación. Como sustento, se exponen los resultados del estudio, que comprenden análisis de correlación, modelos de regresión y evaluaciones de componentes principales.")
+   st.write("El propósito subyacente de este proyecto es robustecer los procesos de observación, control e intervención relacionados con la corrupción. Se concibe como un recurso valioso, sumándose a las iniciativas destinadas a mitigar los efectos adversos de la corrupción en el desarrollo económico. Selecciona cualquiera de las secciones disponibles para explorar a fondo los resultados o para adentrarte en el análisis detallado de los datos.")
+   
+   st.header("RESUMEN")
+   st.write("En este análisis, empleamos diversos indicadores del Banco Mundial que se centran en la gobernanza y el desempeño gubernamental.")
+   st.write("Control de la Corrupción (CC.EST): Evalúa la percepción de cómo el poder público se ejerce para beneficio privado, abarcando diversas formas de corrupción.")
+   st.write("Efectividad del Gobierno (GE.EST): Mide la calidad de los servicios públicos, la independencia del servicio civil y la credibilidad del compromiso gubernamental con sus políticas.")
+   st.write("Estado de Derecho - Cumplimiento de la ley (RL.EST): Refleja la confianza y el cumplimiento de las reglas de la sociedad, incluyendo la aplicación de contratos, derechos de propiedad, la actuación policial y judicial, así como la probabilidad de crimen y violencia")
+   st.write("Voz y Rendición de Cuentas (VA.EST): Evalúa la participación ciudadana en la selección del gobierno y la libertad de expresión, asociación y medios de comunicación.")
+   st.write("Al realizar un modelo de regresión lineal con estos indicadores, se identificaron relaciones significativas entre las variables independientes y el Producto Interno Bruto (PIB) per cápita ajustado por paridad de poder adquisitivo (SL.GDP.PCAP.EM.KD).")
+   st.write("A pesar de que se encontraron fuertes correlaciones entre las variables para el caso de Colombia, es crucial señalar que el modelo no presenta un alto nivel predictivo para estos datos, posiblemente debido a factores únicos en el contexto colombiano no completamente capturados por los indicadores utilizados. A pesar de esta limitación, las fuertes correlaciones entre los indicadores de gobernanza y el PIB per cápita sugieren que mejoras en el control de la corrupción, la efectividad gubernamental, el estado de derecho y la voz ciudadana podrían tener implicaciones positivas en el desarrollo económico de un país.")
 
 with tab3:
+   st.write("En esta sección puedes elegir los parámetros de Pais, Indicador y Año, para conocer al detalle el comportamiento de los indicadores tratados para los países en un margen de tiempo")
    # Ruta del archivo Excel
    file_path = "Extraccion/structured_data/reduced_df_normalized.xlsx"
 
@@ -59,35 +70,33 @@ with tab3:
    #st.dataframe(filtered_df)
 
 with tab2:
-   st.header("RESULTADOS")
-   tab1, tab2, tab3, tab4 = st.tabs(["Correlacion", "Dispersion" , "Resultados Modelos de Regresión" , "Análisis Componentes Principales"])
-   with tab1:
-      st.header("Tabla Matriz de Correlación")
-      tab1.subheader("De acuerdo a la condición de normalidad de las variables analizadas, se realizan los análisis de correlación Spearman o Pearson según el resultado obtenido en cada una. En todos los análisis se determina la correlación entre la variable objetivo identificada y las variables de corrupción más relevantes identificadas a partir de la matriz de correlación.")
-      ruta_matriz_correlacion = r'Procesamiento/graficas_sl/matriz_correlacion.csv'
-      matriz_correlacion = pd.read_csv(ruta_matriz_correlacion, index_col=0)
-      # Aplicar estilos para resaltar valores
-      estilos = matriz_correlacion.style.background_gradient(cmap='coolwarm').highlight_null('red')
-      st.dataframe(estilos)
-      # Mostrar la aplicación Streamlit
+   #st.header("RESULTADOS")
+   tab1, tab2, tab3, tab4, tab5 = st.tabs(["Dispersiòn", "Normalidad","Correlaciòn" , "Resultados Modelos de Regresión" , "Análisis Componentes Principales"])
+   with tab3:
+      st.header("Resultados - Análisis de correlación")
+      st.write("Los indicadores de corrupción son definidos como variables independientes, buscando sus relaciones más fuertes con aquellos que forman parte de los Objetivos de Desarrollo Global y de Desarrollo Sostenible (ODS #8). Mediante una matriz de correlación, identificamos que, entre todos los indicadores evaluados, el Producto Interno Bruto (PIB) per cápita ajustado por paridad de poder adquisitivo (SL.GDP.PCAP.EM.KD) mantiene una relación positiva y sólida con varios indicadores de corrupción. Esta conclusión se valida al analizar el conjunto de datos específico para Colombia.")
       st.write('Matriz de Correlación, entre variables de interes')
       st.image("Procesamiento/graficas_sl/correlación.png", width=900)
 
    with tab2:
+      st.header("Resultados - Comprobación de normalidad")
+      st.write("Para las variables extraídas se realiza una prueba de normalidad, comprobando que todas las variables del conjunto global de datos no se comportan de manera normal. Debido a que posteriormente el modelo de regresión no obtuvo valor predictivo para el subconjunto de datos de Colombia, no se profundizaron las relaciones específicas en este segmento de datos.")
+      ruta_csv = r'Procesamiento/graficas_sl/normal_concatenado.csv'
+      # Cargar el DataFrame desde el archivo CSV
+      norm_concatenado = pd.read_csv(ruta_csv)
+      # Mostrar el DataFrame en Streamlit
+      st.dataframe(norm_concatenado)
+      
+
+   with tab1:
       st.header("Grafica de Dispersion entre variables")
-      tab2.subheader ("")
+      st.write ("Para entender la relación entre las variables, se presentan gráficos de dispersión que conectan cada variable independiente con la variable objetivo. Estos gráficos sugieren una tendencia de relación directa entre la corrupción y el desarrollo económico. En resumen, mejores condiciones en la lucha contra la corrupción están asociadas con un mayor desarrollo económico.")
       st.image("Procesamiento/graficas_sl/diagrama_dispersión.png", width=900)
 
-   with tab4:
+   with tab5:
       st.header("Análisis componentes principales")
       st.write("El hecho de que con 2 o 3 componentes principales se explique más del 95% de la varianza sugiere que estos componentes capturan la mayoría de la información de las variables de corrupción. El gráfico de varianza explicada acumulativa es útil para determinar cuántos componentes son necesarios para conservar una cantidad significativa de varianza.")
-      st.write("El hecho de observar dos clusters distintos en el gráfico de resultados del PCA sugiere que estos clusters se deban a patrones o estructuras que los componentes principales han identificado. Podrían representar grupos o tendencias específicas.")
-      st.write("Los pesos de los componentes principales indican la contribución de cada variable original a los componentes principales. En tu caso, el primer componente principal (PC1) tiene pesos relativamente altos para todas las variables, indicando que está capturando información general de todas ellas. Por otro lado, el segundo componente principal (PC2) tiene un peso significativamente alto para la variable 'CRP_GE.EST' y un peso negativo para 'CRP_VA.EST', lo que sugiere que PC2 podría estar relacionado con variaciones específicas en estas dos variables.")
-      st.write("Los scores de los componentes principales representan las proyecciones de los datos originales en el espacio de los componentes principales. Los valores más altos o más bajos en los scores indican la posición relativa de cada observación en el espacio de los componentes principales:")
-      tab4.subheader("Componente Principal 1 (PC1):")
-      st.write("Este componente parece capturar una tendencia general o patrón común en todas las variables.")
-      tab4.subheader("Componente Principal 2 (PC2)")
-      st.write("Este componente parece capturar variaciones específicas relacionadas con 'CRP_GE.EST' y 'CRP_VA.EST'.")
+      st.write("En el análisis de componentes principales se encontró que los indicadores de corrupción conservan la mayor cantidad de información por medio de 2 componentes principales, el primero con pesos generalmente altos para todas las variables y el segundo tiene un peso significativamente alto para la variable 'CRP_GE.EST' y un peso negativo para 'CRP_VA.EST', lo que sugiere que el segundo componente podría estar relacionado con variaciones específicas en estas dos variables.")
       tab1, tab2 = st.tabs(["📈 Grafica-1", "📈 Grafica-2"])
       with tab1:
          tab1.subheader("Varianza Explicada Acumulativa")
@@ -96,48 +105,40 @@ with tab2:
          tab2.subheader("Resultados PCA")
          st.image("Procesamiento/graficas_sl/Resultados_pca.png", width=900)
  
-   with tab3:
-      tab3.subheader("Resultado - Tabla comparativa entre modelos empleados")
+   with tab4:
+      st.write("Los modelos con mayor capacidad predictiva se identificaron al utilizar el conjunto de datos completo, incorporando la variable país como una categoría esencial. Estos modelos demostraron su eficacia al prever la variable dependiente, especialmente cuando se entrenaron con datos desde 1996 hasta 2002, extendiendo su capacidad predictiva a partir de 1997. Aunque el método de random forest mostró una precisión superior al incluir la variable país, su capacidad predictiva sigue siendo inferior en comparación con los modelos basados en regresión lineal.")
+      st.write("La tabla de resultados  presenta un resumen de los modelos desarrollados para este análisis, detallando el Error Cuadrático Medio (MSE) -indicador de un mejor ajuste cuando se acerca a cero- y el R2 -indicador de un mejor ajuste cuando se acerca a 1- para cada modelo. Además, se incluyen gráficos que contrastan la variable dependiente original con las predicciones generadas por los modelos.")
+      tab4.subheader("Resultado - Tabla comparativa entre modelos empleados")
       ruta_matriz_modelos = r'Procesamiento/graficas_sl/modelos_df.csv'
       matriz_modelos = pd.read_csv(ruta_matriz_modelos, index_col=0)
       # Aplicar estilos para resaltar valores
       estilos = matriz_modelos.style.background_gradient(cmap='coolwarm').highlight_null('red')
       st.dataframe(matriz_modelos)
-      st.header("Resultados modelos de Regresión")
-      st.write ("En este análisis, hemos utilizado una serie de indicadores del Banco Mundial que se centran en la gobernanza y el desempeño de los gobiernos.")  
-      tab3.subheader ("Control de la Corrupción (CC.EST):")
-      st.write ("Control de la Corrupción evalúa la percepción de hasta qué punto el poder público se ejerce para beneficio privado, abarcando formas tanto menores como mayores de corrupción.")
-      tab4.subheader ("Efectividad del Gobierno (GE.EST):")
-      st.write  ("Efectividad del Gobierno mide la calidad de los servicios públicos, la independencia del servicio civil y la credibilidad del compromiso del gobierno con sus políticas.")
-      tab3.subheader ("Estado de Derecho - Cumplimiento de la ley (RL.EST):")
-      st.write  ("Estado de Derecho refleja la confianza y el cumplimiento de las reglas de la sociedad, incluyendo la aplicación de contratos, derechos de propiedad, la actuación policial y judicial, así como la probabilidad de crimen y violencia.")
-      tab3.subheader ("Voz y Rendición de Cuentas (VA.ESTb4):")
-      st.write ("Voz y Rendición de Cuentas evalúa la participación ciudadana en la selección del gobierno y la libertad de expresión, asociación y medios de comunicación.")
       st.header("Grafica Modelos aplicados")
-      tab3.subheader ("Gráfica de Resultados")
+      tab4.subheader ("Gráfica de Resultados")
       tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["📈 Grafica-1", "📈 Grafica-2", "📈 Grafica-3","📈 Grafica-4", "📈 Grafica-5", "📈 Grafica-6"])
       with tab1:
-         tab1.subheader("Graficas de prueba-1")
+         tab1.subheader("1. COMPARACIÓN DE VALORES REALES Y PREDICHOS POR EL MODELO DE REGRESIÓN, SIN EL PAÍS COMO VARIABLE CATEGÓRICA")
          st.image("Procesamiento/graficas_sl/modelo2_rf_sp.png", width=900)
       
       with tab2:
-         tab2.subheader("Graficas de prueba-2")
+         tab2.subheader("2. COMPARACIÓN DE VALORES REALES Y PREDICHOS POR EL MODELO DE REGRESIÓN, CON EL PAÍS COMO VARIABLE CATEGÓRICA")
          st.image("Procesamiento/graficas_sl/modelo2_rf_cp.png", width=900)
       
       with tab3:
-         tab3.subheader("Graficas de prueba-3")
+         tab3.subheader("3. COMPARACIÓN DE VALORES REALES Y PREDICHOS POR EL MODELO DE REGRESIÓN, CON EL PAÍS COMO VARIABLE CATEGÓRICA, CON DATOS DE 1996 A 2002")
          st.image("Procesamiento/graficas_sl/modelo2_cp_pred.png", width=900)
       
       with tab4:
-         tab4.subheader("Graficas ed prueba-4")
+         tab4.subheader("4. COMPARACIÓN DE VALORES REALES Y PREDICHOS POR EL MODELO DE REGRESIÓN, CON EL PAÍS COMO VARIABLE CATEGÓRICA, ENTRENADO CON DATOS ANTES DE 2002 Y PROBADO CON DATOS A PARTIR DEL 2003.")
          st.image("Procesamiento/graficas_sl/modelo2_cp_test.png", width=900)
 
       with tab5:
-         tab5.subheader("Graficas de prueba-5")
+         tab5.subheader("5. COMPARACIÓN DE VALORES REALES Y PREDICHOS POR EL MODELO RANDOM FOREST, SIN EL PAÍS COMO VARIABLE CATEGÓRICA")
          st.image("Procesamiento/graficas_sl/modelo2_rf_sp.png", width=900)
 
       with tab6:
-         tab6.subheader("Graficas de prueba-6")
+         tab6.subheader("6. COMPARACIÓN DE VALORES REALES Y PREDICHOS POR EL MODELO DE REGRESIÓN, CON EL PAÍS COMO VARIABLE CATEGÓRICA")
          st.image("Procesamiento/graficas_sl/modelo2_rf_cp.png", width=900)
 
  
